6dfeb8bfa350bf2d52405533967b03e8ba942272
sd-community1 <xvvv99634@gmail.com>
2026-05-15 02:26:22 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,40 @@
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 
-plugins {
-    id("com.android.library")
-    kotlin("android")
-    id("com.lagradost.cloudstream3.gradle")
+// 1. تعريف من أين يحمل البرنامج الأدوات (Plugins)
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+    }
 }
 
+// 2. تفعيل الأدوات
+apply(plugin = "com.android.library")
+apply(plugin = "kotlin-android")
+apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+// 3. إعدادات الأندرويد
 android {
     namespace = "com.iraq.cee"
     compileSdk = 33
+    
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
 
-cloudstream {
-    // إعدادات بناء الإضافة
+// 4. إعدادات كلودستريم
+configure<CloudstreamExtension> {
+    // يمكنك تركها فارغة حالياً
 }
